ed1a4ada5aa6cadc0057358fbd8c72da6c8bd9a9
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-05-31 16:40:16 +0530

    RMX3031: overlay: Show hidden files and internal storage by default

--- device.mk
+++ device.mk
@@ -135,7 +135,8 @@ PRODUCT_PACKAGES += \
     SettingsOverlay \
     TelephonyOverlay \
     DialerOverlay \
-    NotchBarKiller
+    NotchBarKiller \
+    DocumentsUIOverlay
 
 # Enforce RRO targets
 PRODUCT_ENFORCE_RRO_TARGETS := *
